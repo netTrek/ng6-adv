1d173dd39a26feb9610a60dc6dab4e92f639f5a4
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-11 23:49:36 +0200

    012_createEmbeddedView

--- src/app/dynamic/user-ng-template-outlet/user-ng-template-outlet.component.ts
+++ src/app/dynamic/user-ng-template-outlet/user-ng-template-outlet.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, EmbeddedViewRef, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'post-user-ng-template-outlet',
   templateUrl: './user-ng-template-outlet.component.html',
   styleUrls: ['./user-ng-template-outlet.component.scss']
 })
-export class UserNgTemplateOutletComponent implements OnInit {
+export class UserNgTemplateOutletComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild( TemplateRef )
+  tempRef: TemplateRef<HTMLHRElement>;
+
+  @ViewChild( 'saban', {read: TemplateRef} )
+  tempRef2: TemplateRef<any>;
+
+  constructor( private viewContainerRef: ViewContainerRef ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit (): void {
+
+    const embeddedViewRef: EmbeddedViewRef<HTMLHRElement>  =
+            this.viewContainerRef.createEmbeddedView ( this.tempRef );
+
+    const embeddedViewRef2: EmbeddedViewRef<HTMLHRElement> =
+            this.tempRef2.createEmbeddedView( null );
+    this.viewContainerRef.insert( embeddedViewRef2 );
+
+
+    console.log ( this.viewContainerRef, this.tempRef );
   }
 
 }
