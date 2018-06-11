43486cd370023ceac89766ba6aa67cc967c9aa70
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-11 22:05:18 +0200

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('post-root h1')).getText();
+  }
+}
