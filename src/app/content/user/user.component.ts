e008fdbcdb07fcb6c9df9fcefd7a47d00c74e5bf
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-11 23:08:56 +0200

    007-contentChild

--- src/app/content/user/user.component.ts
+++ src/app/content/user/user.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ElementRef, OnInit, ViewContainerRef } from '@angular/core';
+import { UserIconComponent } from '../user-icon/user-icon.component';
 
-@Component({
-  selector: 'post-user',
+@Component ( {
+  selector   : 'post-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
-})
-export class UserComponent implements OnInit {
+  styleUrls  : [ './user.component.scss' ]
+} )
+export class UserComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  @ContentChild ( UserIconComponent )
+  userIcon: UserIconComponent;
 
-  ngOnInit() {
+  @ContentChild ( UserIconComponent, {read: ElementRef} )
+  userIconElemRef: ElementRef;
+
+  @ContentChild ( UserIconComponent, {read: ViewContainerRef} )
+  userIconViewContainerRef: ViewContainerRef;
+
+  constructor () {
+  }
+
+  ngOnInit () {
+  }
+
+  ngAfterContentInit (): void {
+    console.log ( this.userIcon );
+    console.log ( this.userIconElemRef );
+    console.log ( this.userIconViewContainerRef );
   }
 
 }
