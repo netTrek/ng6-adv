838f7fb5b7739cbadd7145ee57d5dc78f0aba4dc
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-12 00:45:02 +0200

    021_detach

--- src/app/detection/push/push.component.ts
+++ src/app/detection/push/push.component.ts
@@ -6,14 +6,16 @@ import { scan } from 'rxjs/operators';
   selector: 'post-push',
   templateUrl: './push.component.html',
   styleUrls: ['./push.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PushComponent implements OnInit, OnDestroy {
 
+  count: number;
   count$: Subject<number> = new Subject<number>();
   private subscriptions: Subscription[] = [];
 
   constructor ( private cdr: ChangeDetectorRef ) {
+    cdr.detach();
   }
 
   ngOnInit () {
@@ -22,7 +24,8 @@ export class PushComponent implements OnInit, OnDestroy {
         scan ( acc => acc + 1, 0 )
       )
       .subscribe ( next => {
-        this.count$.next( next );
+        this.count = next;
+        // this.count$.next( next );
       } ) );
   }
 
