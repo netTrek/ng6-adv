b437b8eef1863bd841e1dec955683cc29636a43b
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-11 23:33:22 +0200

    010_ViewChildren&Renderer2

--- src/app/views/user/user.component.ts
+++ src/app/views/user/user.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { UserIconComponent } from './user-icon/user-icon.component';
 
 @Component({
@@ -8,19 +8,23 @@ import { UserIconComponent } from './user-icon/user-icon.component';
 })
 export class UserComponent implements OnInit, AfterViewInit {
 
-  @ViewChild ( UserIconComponent )
-  userIcon: UserIconComponent;
+  @ViewChild ( UserIconComponent, {read: ElementRef} )
+  userIcon: ElementRef;
+
+  @ViewChildren ( UserIconComponent )
+  userIcons: QueryList<UserIconComponent>;
 
   @ViewChild ( 'txt' )
   txt: ElementRef;
 
-  constructor() { }
+  constructor( private renderer: Renderer2 ) { }
 
   ngOnInit() {
   }
 
   ngAfterViewInit (): void {
-    console.log ( this.userIcon, this.txt );
+    console.log ( this.userIcon, this.txt, this.userIcons.toArray(), this.renderer );
+    this.renderer.setStyle( this.userIcon.nativeElement, 'color', 'red' );
   }
 
 }
