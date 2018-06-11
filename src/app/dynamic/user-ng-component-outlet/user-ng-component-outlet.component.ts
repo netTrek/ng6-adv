ebeb85c8c6b43b8d4646d6014d8ab4f3bdea6a00
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-12 00:01:12 +0200

    014_createComponent

--- src/app/dynamic/user-ng-component-outlet/user-ng-component-outlet.component.ts
+++ src/app/dynamic/user-ng-component-outlet/user-ng-component-outlet.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactory, ComponentFactoryResolver, ComponentRef, OnInit, ViewContainerRef } from '@angular/core';
 import { MyEntryComponent } from '../my-entry/my-entry.component';
 import { Type } from '@angular/core/src/type';
 
-@Component({
-  selector: 'post-user-ng-component-outlet',
+@Component ( {
+  selector   : 'post-user-ng-component-outlet',
   templateUrl: './user-ng-component-outlet.component.html',
-  styleUrls: ['./user-ng-component-outlet.component.scss']
-})
+  styleUrls  : [ './user-ng-component-outlet.component.scss' ]
+} )
 export class UserNgComponentOutletComponent implements OnInit {
   dynamicComponentClass: Type<MyEntryComponent> = MyEntryComponent;
 
-  constructor() { }
+  constructor ( private componentFactoryResolver: ComponentFactoryResolver,
+                private viewContainerRef: ViewContainerRef ) {
+  }
 
-  ngOnInit() {
+  ngOnInit () {
+    const compFactory: ComponentFactory<MyEntryComponent> =
+            this.componentFactoryResolver.resolveComponentFactory ( MyEntryComponent );
+
+    const dynRef: ComponentRef<MyEntryComponent>      =
+            this.viewContainerRef.createComponent<MyEntryComponent> ( compFactory );
+
   }
 
 }
