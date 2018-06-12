54fd5e6cc56007d90c0e329060aea51c0f1c7f9d
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-12 11:51:40 +0200

    001_env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
-@Component({
-  selector: 'post-root',
+@Component ( {
+  selector   : 'post-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  styleUrls  : [ './app.component.scss' ]
+} )
 export class AppComponent {
   title = 'post';
+
+  constructor () {
+    console.log ( environment.endpoint );
+  }
 }
