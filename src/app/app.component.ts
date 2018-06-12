6d610d3ccb1964542f4917792b1fc8a7fe868abb
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-12 14:35:46 +0200

    003_pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component ( {
   selector   : 'post-root',
   templateUrl: './app.component.html',
   styleUrls  : [ './app.component.scss' ]
 } )
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'post';
 
-  constructor () {
+  constructor ( private swUpdate: SwUpdate ) {
     console.log ( environment.endpoint );
+  }
+
+  ngOnInit (): void {
+    if ( this.swUpdate.isEnabled ) {
+
+      this.swUpdate.available.subscribe (
+        event => {
+          if ( confirm('New version available, should I load?') ) {
+            this.swUpdate.activateUpdate()
+              .then( () => {
+                document.location.reload();
+              });
+          }
+        }
+      );
+
+      this.swUpdate.checkForUpdate();
+      interval( 5000 )
+        .subscribe( () => {
+          this.swUpdate.checkForUpdate();
+        });
+    }
+
   }
 }
