d5b848b956364fa4bb3c3c0e5c34e544264a3c55
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-14 16:48:59 +0200

    css validatoren

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, NgForm } from '@angular/forms';
 
-@Component({
-  selector: 'post-contact-form',
+@Component ( {
+  selector   : 'post-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
-})
+  styleUrls  : [ './contact-form.component.scss' ]
+} )
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  initial = { name: '', credentials: { email: 'us@netTrek.de', password: '' } };
 
-  ngOnInit() {
+  constructor () {
   }
 
+  ngOnInit () {
+  }
+
+  chg () {
+    this.initial.credentials.email = 'info@netTrek.de';
+  }
+
+  submitForm ( myForm: AbstractControl ) {
+    console.log ( myForm.value );
+  }
+
+  resetForm ( myForm: NgForm, event?: Event ) {
+    event.stopPropagation ();
+    event.preventDefault ();
+    myForm.setValue( this.initial );
+    /*
+    myForm.controls.name.setValue ( this.initial.name );
+    (myForm.controls.credentials as FormGroup).controls.email.setValue ( this.initial.credentials.email );
+    (myForm.controls.credentials as FormGroup).controls.password.setValue ( this.initial.credentials.password );
+    */
+  }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -5,11 +5,13 @@ import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact/contact.component';
 import { ContactMapComponent } from './contact-map/contact-map.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    FormsModule
   ],
   declarations: [ContactComponent, ContactMapComponent, ContactFormComponent]
 })
