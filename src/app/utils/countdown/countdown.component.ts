4375d6a3c188e6e81acc05dc608fe101242d50b1
Saban Uenlue - netTrek <us@netTrek.de>
2018-08-02 16:45:59 +0200

    004-styleleBinden

--- src/app/utils/countdown/countdown.component.ts
+++ src/app/utils/countdown/countdown.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pr-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent implements OnInit {
-
-  progress = 100;
-
+export class CountdownComponent implements  OnInit, OnDestroy {
+  progress = 100; // aktueller Wert
+  private countdown = 5; // zeit bis Ende des CountDowns
+  private time = 0; // zeit wie lande CountDown schon läuft
+  private intervalID: number; // intervalID
+  private isDemoMod = true; // Demo = true => Endlos Schleife
   constructor() { }
-
   ngOnInit() {
+    this.startCountDown ();
   }
-
+  private startCountDown () {
+    this.intervalID = window.setInterval ( () => {
+      this.progress = Math.round( ( 1 - (++ this.time / this.countdown)) * 100 );
+      if ( this.time === this.countdown ) {
+        this.stopCountDownAndReset ();
+      }
+    }, 1000 );
+  }
+  ngOnDestroy (): void {
+    this.stopCountDownAndReset ( true ); // wenn true erzwinge stop auch in Demo
+  }
+  private stopCountDownAndReset ( force: boolean = false ) {
+    window.clearInterval ( this.intervalID );
+    this.intervalID = undefined;
+    this.countdown = 10;
+    this.time = 0;
+    if ( this.isDemoMod && !force ) {
+      this.startCountDown();
+    }
+  }
 }
