6a1ada83ec7a07ee3184b8e298ad7c365ecc5f8e
Saban Uenlue - netTrek <us@netTrek.de>
2018-08-02 15:04:00 +0200

    002 - viewChild

--- src/app/user/user-list/user-list-item/user-list-item.component.ts
+++ src/app/user/user-list/user-list-item/user-list-item.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  OnInit,
+  Query,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+  ViewEncapsulation
+} from '@angular/core';
+import { UserAvatarComponent } from '../../user-avatar/user-avatar.component';
 
 @Component({
   selector: 'pr-user-list-item',
   templateUrl: './user-list-item.component.html',
   styleUrls: ['./user-list-item.component.scss']
 })
-export class UserListItemComponent implements OnInit {
+export class UserListItemComponent implements OnInit, AfterViewInit {
+
+  @ViewChild ( UserAvatarComponent )
+  avatar: UserAvatarComponent;
+
+  @ViewChildren ( UserAvatarComponent )
+  avatars: QueryList<UserAvatarComponent>;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit (): void {
+    console.log ( this.avatar );
+    console.log ( this.avatars.toArray() );
+    // this.avatars.changes.subscribe( value => { console.log ( value );} );
   }
 
 }
