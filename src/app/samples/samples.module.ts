735284c3d20c8dbb13dafbe66e1fde907efb30ac
Saban Uenlue - netTrek <us@netTrek.de>
2018-08-06 13:54:59 +0200

    010-pipes

--- src/app/samples/samples.module.ts
+++ src/app/samples/samples.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PipeSamplesComponent } from './pipe-samples/pipe-samples.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { FillStatePipe } from './pipes/fill-state.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PipeSamplesComponent],
-  exports: [PipeSamplesComponent]
+  declarations: [PipeSamplesComponent, ReversePipe, FillStatePipe],
+  exports: [PipeSamplesComponent, ReversePipe, FillStatePipe]
 })
 export class SamplesModule { }
