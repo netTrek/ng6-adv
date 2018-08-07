58c8579951bd3e96e062a29fd58f9d33b817ed86
Saban Uenlue - netTrek <us@netTrek.de>
2018-08-07 11:32:15 +0200

    014-http-service-rxjs

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,13 +1,34 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable ( {
   providedIn: 'root'
 } )
 export class UserService {
 
-  selectedIndex    = - 1;
-  userList: User[] = [
+  selectedIndex$: BehaviorSubject<number> = new BehaviorSubject<number>( -1 );
+
+  set selectedIndex ( index: number ) {
+    this.selectedIndex$.next( index );
+  }
+
+  get selectedIndex (): number {
+    return this.selectedIndex$.getValue();
+  }
+
+  selectedUsr$: BehaviorSubject<User> = new BehaviorSubject<User>( undefined );
+
+  set selectedUsr ( index: User ) {
+    this.selectedUsr$.next( index );
+  }
+
+  get selectedUsr (): User {
+    return this.selectedUsr$.getValue();
+  }
+
+
+  userList$: BehaviorSubject<User[]> = new BehaviorSubject<User[]>(  [
     {
       'id'       : 1,
       'firstname': 'saban',
@@ -26,15 +47,25 @@ export class UserService {
       'lastname' : 'maier',
       'birthday' : '1973-11-04'
     }
-  ];
-  selectedUsr: User;
+  ]);
+
+  set userList ( index: User[] ) {
+    this.userList$.next( index );
+  }
+
+  get userList (): User[] {
+    return this.userList$.getValue();
+  }
+
 
   constructor () {
   }
 
   selectNextUsr () {
-    if ( ++ this.selectedIndex === 4 ) {
+    if ( this.selectedIndex + 1 === this.userList.length ) {
       this.selectedIndex = 0;
+    } else {
+      ++ this.selectedIndex;
     }
   }
 
@@ -48,13 +79,14 @@ export class UserService {
   }
 
   addUser ( user: User ) {
-    this.userList.push ( user );
+    this.userList$.next( [ ...this.userList, user ] );
     this.selectedUsr   = user;
     this.selectedIndex = this.userList.length - 1;
   }
 
   delUsr ( user: User ) {
     this.userList.splice ( this.userList.indexOf ( user ), 1 );
+    this.userList$.next( [ ...this.userList ] );
     this.selectedUsr   = undefined;
     this.selectedIndex = - 1;
   }
