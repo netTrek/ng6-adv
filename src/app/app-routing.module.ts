7ad3fef48444e6af2890c483423833735e6d346e
Saban Uenlue - netTrek <us@netTrek.de>
2018-08-07 20:49:04 +0200

    015-routing-config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { CountdownComponent } from './utils/countdown/countdown.component';
+import { RxjsSamplesComponent } from './samples/rxjs-samples/rxjs-samples.component';
+import { PipeSamplesComponent } from './samples/pipe-samples/pipe-samples.component';
+import { PizzasComponent } from './samples/pizzas/pizzas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'users'
+  },
+  {
+    path: 'users',
+    component: UserListComponent
+  },
+  {
+    path: 'countdown',
+    component: CountdownComponent
+  },
+  {
+    path: 'rxjs',
+    component: RxjsSamplesComponent
+  },
+  {
+    path: 'pipe',
+    component: PipeSamplesComponent
+  },
+  {
+    path: 'di',
+    component: PizzasComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'users'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/utils/countdown/countdown.component.ts
+++ src/app/utils/countdown/countdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pr-countdown',
@@ -10,29 +10,8 @@ export class CountdownComponent implements  OnInit, OnDestroy {
   private countdown = 5; // zeit bis Ende des CountDowns
   private time = 0; // zeit wie lande CountDown schon läuft
   private intervalID: number; // intervalID
-  private isDemoMod = true; // Demo = true => Endlos Schleife
-  constructor() { }
-  ngOnInit() {
-    this.startCountDown ();
-  }
-  private startCountDown () {
-    this.intervalID = window.setInterval ( () => {
-      this.progress = Math.round( ( 1 - (++ this.time / this.countdown)) * 100 );
-      if ( this.time === this.countdown ) {
-        this.stopCountDownAndReset ();
-      }
-    }, 1000 );
-  }
-  ngOnDestroy (): void {
-    this.stopCountDownAndReset ( true ); // wenn true erzwinge stop auch in Demo
-  }
-  private stopCountDownAndReset ( force: boolean = false ) {
-    window.clearInterval ( this.intervalID );
-    this.intervalID = undefined;
-    this.countdown = 10;
-    this.time = 0;
-    if ( this.isDemoMod && !force ) {
-      this.startCountDown();
-    }
-  }
-}
+  private isDemoMod = true; // Demo = true =>
+
+  @HostBinding ('style.width.%')
+  private width = 100;
+…
