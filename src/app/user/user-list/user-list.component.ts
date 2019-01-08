e8ff0caf535b332ca9b655e503de184e0b6f4e61
Saban Uenlue - netTrek <us@netTrek.de>
2019-01-08 11:37:09 +0100

    ViewChild und Renderer

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,4 +1,14 @@
-import { AfterContentInit, AfterViewInit, Component, ContentChild, ContentChildren, OnInit, QueryList } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  Renderer2,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 import { UserListHeaderComponent } from './user-list-header/user-list-header.component';
 import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
@@ -7,24 +17,34 @@ import { UserListItemComponent } from './user-list-item/user-list-item.component
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent implements OnInit, AfterContentInit {
+export class UserListComponent implements OnInit, AfterViewInit {
 
-  @ContentChild( UserListHeaderComponent )
+  @ViewChild( 'line' )
+  line: ElementRef;
+
+  @ViewChild( UserListHeaderComponent )
   header: UserListHeaderComponent;
 
-  @ContentChildren( UserListItemComponent )
+  @ViewChildren( UserListItemComponent )
   items: QueryList<UserListItemComponent>;
 
-  constructor() {
+  constructor( private renderer: Renderer2 ) {
   }
 
   ngOnInit() {
   }
 
-  ngAfterContentInit (): void {
+  ngAfterViewInit (): void {
     console.log ( this.header );
     console.log ( this.items );
     console.log ( this.items.toArray() );
+    console.log ( this.line );
+    console.log ( this.line.nativeElement );
+
+    // this.line.nativeElement.style.height = '3px';  // nur ohne universal - platform-server
+    // this.line.nativeElement.style.backgroundColor = 'red';  // nur ohne universal - platform-server
+    // this.line.nativeElement.style.borderColor = 'red'; // nur ohne universal - platform-server
+    this.renderer.setStyle( this.line.nativeElement , 'borderColor', 'red');
   }
 
 }
