5aa53489ebdf998f09e85665264dbaf9b79c4b43
Saban Uenlue - netTrek <us@netTrek.de>
2019-01-23 14:31:11 +0100

    class Bindungen

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
+  selectedIndex = -1;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  setSelectedIndex ( ind: number ) {
+    if ( ind === this.selectedIndex ) {
+      this.selectedIndex = -1;
+    } else {
+      this.selectedIndex = ind;
+    }
+  }
 }
