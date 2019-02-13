c0c54062a84a1724cd5dcf16ae3e6230118487c1
Saban Uenlue - netTrek <us@netTrek.de>
2019-02-13 16:24:40 +0100

    ViewChild, ElementRef und Renderer

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { AvatarComponent } from '../avatar/avatar.component';
 import { UserComponent } from '../user/user.component';
 
@@ -9,19 +9,26 @@ import { UserComponent } from '../user/user.component';
 })
 export class UserListComponent implements OnInit, AfterViewInit {
 
+  @ViewChild ( 'line' )
+  line: ElementRef<HTMLHRElement>
+
   @ViewChild ( AvatarComponent )
   avatar: AvatarComponent;
 
   @ViewChildren( UserComponent )
   users: QueryList<UserComponent>;
 
-  constructor() { }
+  constructor( private renderer: Renderer2 ) { }
 
   ngOnInit() {
   }
 
   ngAfterViewInit(): void {
     console.log ( this.avatar );
+    // auch unter SSR ( Angular Universal )
+    this.renderer.setStyle( this.line.nativeElement, 'borderColor', 'blue' );
+    // nicht möglich bei SSR ( Angular Universal )
+    // this.line.nativeElement.style.borderColor = 'blue';
   }
 
 }
